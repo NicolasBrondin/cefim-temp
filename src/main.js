03d505e328358d39a2bd5f3be162e0ff2359702d
Nicolas Brondin-Bernard <nicolas@brondin.com>
2021-07-09 14:20:22 +0200

    [FEATURE] PWA - Added service worker for offline feature

--- src/main.js
+++ src/main.js
@@ -2,6 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import './index.css';
 
+
+/* Work offline */
+// Register the service worker
+if ('serviceWorker' in navigator) {
+  // Wait for the 'load' event to not block other work
+  window.addEventListener('load', async () => {
+    // Try to register the service worker.
+    try {
+      const reg = await navigator.serviceWorker.register('./sw.js');
+      console.log('Service worker registered! 😎', reg);
+    } catch (err) {
+      console.log('😥 Service worker registration failed: ', err);
+    }
+  });
+}
+
 Vue.config.productionTip = false
 
 new Vue({
